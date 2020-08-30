14d9c1e5a85f872d931bb6ed40a25f0e05476549
dyaksa <diasnour0395@gmail.com>
2020-08-30 18:24:46 +0700

    adding text format and css drawer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-import './styles/main.css';
+import "./styles/main.css";
+
+const menu = document.getElementById("menu");
+const drawer = document.getElementById("drawer");
+const main = document.querySelector("main");
+const hero = document.querySelector(".hero");
+
+menu.addEventListener("click", (event) => {
+  drawer.classList.toggle("open");
+  console.log(drawer);
+  event.stopPropagation();
+});
+
+main.addEventListener("click", (event) => {
+  drawer.classList.remove("open");
+});
+
+hero.addEventListener("click", (event) => {
+  drawer.classList.remove("open");
+});
